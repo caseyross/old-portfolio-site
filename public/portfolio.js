38499fd28c408d173514268b12914f1967a2a2e8
Casey Ross <caseyros@gmail.com>
2015-11-04 07:37:40 +0000

    Add more commentary

--- public/portfolio.js
+++ public/portfolio.js
@@ -20,39 +20,42 @@
         year: '2012',
         type: 'school',
         title: 'Simulation and visualization of elevator movement',
+        commentary: '',
         technologies: ['java', 'swing'],
         images: ['elevator3_jfhjsg', 'elevator4_btvqol', 'elevator2_st2cox', 'elevator1_bukyyn', 'elevator5_y5qm8t', 'elevator6_tty38t'],
         github: 'http://www.github.com/caseyross/elevator-sim'
       }, {
-        name: 'Portfolio',
+        name: 'Personal website',
         year: '2015',
         type: 'personal',
-        title: 'This website',
-        technologies: ['node.js', 'express', 'angular.js', 'heroku', 'adobe illustrator'],
+        title: 'This page',
+        commentary: 'My primary goal here was to create a museum-like environment to display projects.',
+        technologies: ['node.js', 'express', 'angular.js', 'coffeescript', 'jade', 'heroku', 'adobe illustrator'],
         images: ['ps_k0pccb', 'ps2_cu56fh', 'ps3_xvuptj', 'ps4_touof9', 'portfolio_unddhw.png'],
         github: 'http://www.github.com/caseyross/portfolio'
       }, {
-        name: 'STEMfire redesign',
+        name: 'Volunteer platform redesign',
         year: '2015',
         type: 'school / work',
         title: 'UX evaluation and redesign for stemfire.com',
-        technologies: ['ruby on rails', 'postgresql', 'heroku', 'bootstrap'],
+        commentary: '',
+        technologies: ['ruby on rails', 'bootstrap', 'heroku', 'postgresql', 'elasticsearch'],
         images: ['s_events_qoetlo.jpg', 'sf_sketch_oabn0i', 'w_dashboard_l9me1t', 'w_event_oyymtb', 's_event_jlregq'],
         livelink: 'http://www.stemfire.com'
       }, {
         name: 'T-shirt design',
         year: '2013',
         type: 'personal',
-        title: 'T-shirt design contest entry for my university\'s honors program',
-        commentary: '',
+        title: 'T-shirt design for my university\'s honors program',
+        commentary: 'The somewhat inscrutable motif here is meant to be a stylized top-down view of four people in a group. Since the name of the honors program is inspired by the classical idea of the \'fifth element\', I wanted to communicate how four objects can come together to create a fifth (here, the central star shape). \n \nWhile the figure was probably overly abstract, I think the colors turned out well.',
         technologies: ['adobe illustrator'],
         images: ['CV_TShirt_Contest_Design_dik2oq.png']
       }, {
         name: 'Olympics logo',
         year: '2015',
         type: 'personal',
-        title: 'Just-for-fun logo for the 2020 Olympics',
-        commentary: 'I tried to evoke three ideas with the shapes and arrangement of this logo: \n \n1. The easily recognizable icons of the Olympic rings and the Rising Sun \n2. The silhouette of Mt. Fuji, an internationally renowned Japanese landmark \n3. The sun above the curve of the earth, representing both a bright, optimistic future and a unified, equal gathering of humanity',
+        title: 'Logo for the 2020 Olympics',
+        commentary: 'I tried to evoke three ideas with the shapes and arrangement of this logo: \n \n1. The easily recognizable icons of the Olympic rings and the Rising Sun \n2. The silhouette of Mt. Fuji, an internationally renowned Japanese landmark (this is why the design is asymmetric) \n3. The sun above the curve of the earth, representing both a bright, optimistic future and a unified, equal gathering of humanity',
         technologies: ['adobe illustrator'],
         images: ['tokyo-2020_wkpcc1.png']
       }
